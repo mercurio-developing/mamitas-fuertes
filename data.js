9f196f715a4dff89f07661895008bfd4a623caa6
Alejandro Fidanza <mercuriodevelop@gmail.com>
2020-10-19 17:19:17 -0500

    tag and new episode of Alma

--- data.js
+++ data.js
@@ -2,21 +2,25 @@ export default [
   {
     _id: "001",
     title: "001: Nina",
-    description: "Para empezar este nuevo proyecto voy a compartir la historia del nacimiento de mi hija, Nina.",
     mother_name: "Rebecca Winterfield",
     date: "16 JULIO 2020",
-    // thumbnail: "episode_place_holder.jpg",
     spotify_url: "https://open.spotify.com/episode/2cI3OeQ7jiOlow308YPj1L",
     spotify_player: "https://anchor.fm/mamitasfuertes/embed/episodes/001-Nina-egqnim/a-a2nbpgu"
   },
   {
     _id: "002",
     title: "002: Leon",
-    description: "Para empezar este nuevo proyecto voy a compartir la historia del nacimiento de mi hija, Nina.",
     mother_name: "Lucila Vairo",
     date: "28 SEPTIEMBRE 2020",
-    // thumbnail: "episode_place_holder.jpg",
     spotify_url: "https://open.spotify.com/episode/7FqvvcIdTj8s9K3LGvekOt",
     spotify_player: "https://anchor.fm/mamitasfuertes/embed/episodes/002-Leon-ek9sli"
+  },
+  {
+    _id: "003",
+    title: "003: Alma",
+    mother_name: "Greta Quijano",
+    date: "19 OCTUBREt 2020",
+    spotify_url: "https://open.spotify.com/episode/2wIfLHcuOwFktlhfRBHmY6",
+    spotify_player: "https://anchor.fm/mamitasfuertes/embed/episodes/003-Alma-el9dr1"
   }
 ]
